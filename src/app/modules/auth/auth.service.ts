4c4455505447c6aaeed13dbb1cf8cde6f367a379
iamrazzakk <rakibt23p@gmail.com>
2025-08-31 11:57:25 +0600

    refactor auth service

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -397,10 +397,7 @@ const deleteUserByEmailAndPassword = async (
 
 // get all unapproved users
 const getAllUnapprovedUsersIntoDB = async (query: Record<string, any>) => {
-  const result = new QueryBuilder(
-    User.find({ adminApproval: false, unApprove: false }),
-    query
-  );
+  const result = new QueryBuilder(User.find({ adminApproval: false }), query);
 
   const data = await result.modelQuery;
   const meta = await result.getPaginationInfo();
