92dad0e5b5c238ecbdfa9db0c6275eb0a5e77bab
iamrazzakk <rakibt23p@gmail.com>
2025-12-04 16:03:17 +0600

    refactor some code

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -2,5 +2,6 @@ import Stripe from 'stripe';
 import config from './index';
 
 export const stripe = new Stripe(config.stripe.stripeSecretKey as string, {
+  // @ts-ignore
   apiVersion: '2025-03-31.basil',
 });
